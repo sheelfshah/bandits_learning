ef4e21b47b4a57d9a542486726d9c82ef04f2393
sheelfshah <sheelfshah@gmail.com>
2021-05-09 16:02:12 +0530

    ETC done

--- tor_lattimore_excercises/algorithms.py
+++ tor_lattimore_excercises/algorithms.py
@@ -1,5 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
 
 from bandits import *
 
@@ -21,7 +23,18 @@ def FollowTheLeader(bandit, n):
             bandit.pull(best_arm)
 
 
-if __name__ == '__main__':
+def FTLRun():
+
+    n = 1000
+    regrets = np.zeros(1000)
+    for j in range(1000):
+        bernoulli_bandit = BernoulliBandit([0.5, 0.6])
+        FollowTheLeader(bernoulli_bandit, n)
+        regrets[j] = bernoulli_bandit.regret()
+
+    plt.hist(regrets)
+    plt.show()
+
     ns = [i * 100 for i in range(1, 11)]
     regrets = np.zeros((len(ns), 1000))
     for i, n in enumerate(ns):
@@ -33,3 +46,81 @@ if __name__ == '__main__':
     plt.errorbar(x=ns, y=regrets.mean(axis=1),
                  yerr=regrets.std(axis=1), fmt='-o')
     plt.show()
+
+
+def ExploreThenCommit2(n, delta, m=None):
+    # ETC algo with u1 = 0, u2 = -delta
+    gaussian_bandit = GaussianBandit([0, -delta])
+
+    if m is None:
+        m_val = (4 / (delta * delta)) * \
+            np.log(n * delta * delta / 4)  # from eq 6.5
+        m = max(1, int(np.ceil(m_val)))
+
+    Rn_ub_val = delta + (4 / delta) * \
+        (1 + max(0, np.log(n * delta * delta / 4)))  # from eq 6.6
+    Rn_max = min(n * delta, Rn_ub_val)
+
+    rewards = np.zeros((2, m))
+    rewards[0, :] = gaussian_bandit.pull_repeatedly(0, m)
+    rewards[1, :] = gaussian_bandit.pull_repeatedly(1, m)
+
+    rewards = rewards.mean(axis=1)  # average over m pulls
+
+    best_arm = np.random.choice(
+        np.flatnonzero(rewards == rewards.max()))
+
+    rewards = gaussian_bandit.pull_repeatedly(best_arm, n - 2 * m)
+
+    return Rn_max, gaussian_bandit.regret()
+
+
+def ETC2Run():
+    delta_vals = [i * 0.04 for i in range(1, 25)]
+    ub_regrets = []
+    actual_regrets = []
+    num_runs = 500
+
+    for delta in delta_vals:
+        ub_regret_sum, actual_regret_sum = 0, 0
+        for run_no in range(num_runs):
+            ub_regret, actual_regret = ExploreThenCommit2(n=1000, delta=delta)
+            ub_regret_sum += ub_regret
+            actual_regret_sum += actual_regret
+        ub_regrets.append(ub_regret_sum / num_runs)
+        actual_regrets.append(actual_regret_sum / num_runs)
+
+    plt.plot(delta_vals, ub_regrets, label="Upper Bound")
+    plt.plot(delta_vals, actual_regrets, label="Actual Regret")
+    plt.legend()
+    plt.show()
+
+    m_vals = [i * 15 for i in range(1, 25)]
+    actual_regrets = np.zeros((len(m_vals), num_runs))
+    for i, m in enumerate(m_vals):
+        actual_regret_sum = 0
+        for run_no in range(num_runs):
+            _, actual_regret = ExploreThenCommit2(
+                n=2000, delta=0.1, m=m)
+            actual_regrets[i, run_no] = actual_regret
+
+    plt.plot(m_vals, actual_regrets.mean(axis=1))
+    plt.show()
+
+    plt.plot(m_vals, actual_regrets.std(axis=1))
+    plt.show()
+
+    hue = np.array(m_vals)
+    hue = np.repeat(hue, num_runs)
+    sns.displot(x=actual_regrets.flatten(),
+                hue=hue, kind="kde")
+    plt.show()
+
+if __name__ == '__main__':
+    algo = input("Algorithm name: ")
+
+    if algo == "FTL":
+        FTLRun()
+
+    elif algo == "ETC2":
+        ETC2Run()
--- tor_lattimore_excercises/bandits.py
+++ tor_lattimore_excercises/bandits.py
@@ -6,7 +6,7 @@ class BernoulliBandit:
     # accepts a list of K >= 2 floats , each lying in [0 ,1]
     def __init__(self, means):
         self.arm_means = means
-        self.pseudo_regret = 0
+        self.actual_regret = 0
 
     # Function should return the number of arms
     def K(self):
@@ -17,13 +17,45 @@ class BernoulliBandit:
     # the mean of the (a +1) th arm .
     def pull(self, a):
         reward = np.random.binomial(size=1, n=1, p=self.arm_means[a])
-        self.pseudo_regret += (max(self.arm_means) -
-                               self.arm_means[a])  # pseudo regret
+        self.actual_regret += (max(self.arm_means) -
+                               reward)  # regret
         return reward.item()
 
     # Returns the regret incurred so far .
     def regret(self):
-        return self.pseudo_regret
+        return self.actual_regret
+
+
+class GaussianBandit:
+    # gaussian arms, and actual regret
+
+    # accepts a list of K >= 2 floats
+    def __init__(self, means):
+        self.arm_means = means
+        self.actual_regret = 0
+
+    # Function should return the number of arms
+    def K(self):
+        return len(self.arm_means)
+
+    # Accepts a parameter 0 <= a <= K -1 and returns the
+    # realisation of random variable X with gaussian(mean = ua, std=1)
+    def pull(self, a):
+        reward = np.random.normal(loc=self.arm_means[a], scale=1)
+        self.actual_regret += (max(self.arm_means) - reward)  # regret
+        return reward
+
+    # pulls the same arm repeatedly, to speed things up
+    def pull_repeatedly(self, a, num_reps):
+        rewards = np.random.normal(
+            loc=self.arm_means[a], scale=1, size=(num_reps, ))
+        self.actual_regret += num_reps * \
+            max(self.arm_means) - np.sum(rewards)  # regret
+        return rewards
+
+    # Returns the regret incurred so far .
+    def regret(self):
+        return self.actual_regret
 
 
 if __name__ == '__main__':
